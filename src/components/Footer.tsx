c052bc0d3998f60d88094555ec382c9c2ea01d3c
anmolSoni02 <anmol.soni@vitraya.com>
2025-09-10 17:10:14 +0530

    add csh logo on footer and navigation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ import idg_footer from "@/components/svg/idg_footer.svg"
 import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
-
 import { MdEmail, MdPhone, MdLocationOn } from "react-icons/md";
+import csh_footer from "@/components/svg/cybershields-footer-logo.svg"
 
 
 
@@ -18,29 +18,29 @@ const Footer = () => {
           <div className="space-y-4">
             <div className="flex items-center space-x-2">
               <div className="flex items-center justify-center">
-             <img src={idg_footer} className="h-12 w-auto" alt="" /> 
+                <img src={idg_footer} className="h-12 w-auto" alt="" />
               </div>
             </div>
             <p className="text-gray-400 dark:text-gray-500">
               Empowering careers through digital marketing education and guaranteed job placement support.
             </p>
             <div className="flex space-x-4 pt-4">
-  <a href="https://www.facebook.com/instituteofdigitalstudies/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors text-lg">
-    <FaFacebookF />
-  </a>
-  <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors text-lg">
-    <FaXTwitter />
-  </a>
-  <a href="https://www.instagram.com/instituteofdigitalstudies/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors text-lg">
-    <FaInstagram />
-  </a>
-  <a href="https://www.linkedin.com/company/institute-of-digital-marketing-studies/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors text-lg">
-    <FaLinkedinIn />
-  </a>
-  <a href="hhttps://www.youtube.com/@instituteofdigitalstudies5567" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors text-lg">
-    <FaYoutube />
-  </a>
-</div>
+              <a href="https://www.facebook.com/instituteofdigitalstudies/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors text-lg">
+                <FaFacebookF />
+              </a>
+              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors text-lg">
+                <FaXTwitter />
+              </a>
+              <a href="https://www.instagram.com/instituteofdigitalstudies/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors text-lg">
+                <FaInstagram />
+              </a>
+              <a href="https://www.linkedin.com/company/institute-of-digital-marketing-studies/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors text-lg">
+                <FaLinkedinIn />
+              </a>
+              <a href="hhttps://www.youtube.com/@instituteofdigitalstudies5567" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-white transition-colors text-lg">
+                <FaYoutube />
+              </a>
+            </div>
 
           </div>
 
@@ -67,31 +67,32 @@ const Footer = () => {
           </div>
 
           <div>
-  <h4 className="font-semibold mb-4">Contact Info</h4>
-  <ul className="space-y-2 text-gray-400 dark:text-gray-500">
-    <li className="flex items-start gap-2">
-      <MdLocationOn className="text-xl mt-1" />
-      Business Address: T3, B1603, NXOne, Tech zone 4, Opposite Gaur city mall, Greater Noida, UP, 201306
-    </li>
-    <li className="flex items-start gap-2">
-      <MdLocationOn className="text-xl mt-1" />
-     Registered Address: B-110, GAUR CASCADES, RAJNAGAR EXTN., Ghaziabad, UttarPradesh, 201001
-    </li>
-    <li className="flex items-center gap-2">
-      <MdPhone className="text-xl" />
-      <a href="tel:+919315471293" className="hover:text-white transition-colors">+91 9315471293</a>
-    </li>
-    <li className="flex items-center gap-2">
-      <MdEmail className="text-xl" />
-      <a href="mailto:info@idigitalstudies.com" className="hover:text-white transition-colors">info@idigitalstudies.com</a>
-    </li>
-  </ul>
-</div>
+            <h4 className="font-semibold mb-4">Contact Info</h4>
+            <ul className="space-y-2 text-gray-400 dark:text-gray-500">
+              <li className="flex items-start gap-2">
+                <MdLocationOn className="text-xl mt-1" />
+                Business Address: T3, B1603, NXOne, Tech zone 4, Opposite Gaur city mall, Greater Noida, UP, 201306
+              </li>
+              <li className="flex items-start gap-2">
+                <MdLocationOn className="text-xl mt-1" />
+                Registered Address: B-110, GAUR CASCADES, RAJNAGAR EXTN., Ghaziabad, UttarPradesh, 201001
+              </li>
+              <li className="flex items-center gap-2">
+                <MdPhone className="text-xl" />
+                <a href="tel:+919315471293" className="hover:text-white transition-colors">+91 9315471293</a>
+              </li>
+              <li className="flex items-center gap-2">
+                <MdEmail className="text-xl" />
+                <a href="mailto:info@idigitalstudies.com" className="hover:text-white transition-colors">info@idigitalstudies.com</a>
+              </li>
+            </ul>
+          </div>
 
         </div>
 
-        <div className="border-t border-gray-800 dark:border-gray-700 mt-12 pt-8 text-center text-gray-400 dark:text-gray-500">
-          <p>&copy; {new Date().getFullYear()} Institute of Digital Studies. All rights reserved.</p>
+        <div className="border-t flex justify-between border-gray-800 dark:border-gray-700 mt-12 pt-8 text-gray-400 dark:text-gray-500">
+          <p className="text-left font-semibold">&copy; {new Date().getFullYear()} Institute of Digital Studies(IDS). An initiative under Cybershield Technologies Pvt. Ltd.</p>
+          <Link to = 'https://cybershieldtechnologies.com/' target="blank" rel="noopener noreferrer"><img src={csh_footer} className="h-10 w-auto" alt="" /></Link>
         </div>
       </div>
     </footer>
