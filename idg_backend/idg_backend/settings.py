f96ae29421f6cef7a5b41d5888991f9cff6e4436
Anmol Soni <103637964+Anmolsoni04@users.noreply.github.com>
2025-10-11 18:29:02 +0530

    Update settings.py
    add new ip address for backend

--- idg_backend/idg_backend/settings.py
+++ idg_backend/idg_backend/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fallback-dev-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 # DEBUG = True  #for local
 
-ALLOWED_HOSTS = ['idg-backend.onrender.com']
+# ALLOWED_HOSTS = ['idg-backend.onrender.com']
+ALLOWED_HOSTS = ['16.171.151.203', '*']
+
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] # for local
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
