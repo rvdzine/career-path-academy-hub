0eb87d1a40ebf1d5f04b632cf5c6823c8e9fbad8
anmolSoni02 <anmol.soni@vitraya.com>
2025-12-02 03:23:54 +0530

    build error fixed 5th - 2nd dec 2025

--- nextjs-app/next.config.js
+++ nextjs-app/next.config.js
@@ -1,19 +1,12 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
 const nextConfig = {
   images: {
     domains: [],
     unoptimized: false,
   },
   reactStrictMode: false,
-  outputFileTracingRoot: path.join(__dirname, '../'),
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
+  eslint: { ignoreDuringBuilds: false },
+  typescript: { ignoreBuildErrors: false },
 };
 
 module.exports = nextConfig;
